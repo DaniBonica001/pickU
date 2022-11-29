395dabdd529f2accb4a8a7605b6f9431756eba1c
cosmeticlizard <pipe.barreto07@gmail.com>
2022-11-28 22:13:16 -0500

    merge error

--- screens/RequestScreen.js
+++ screens/RequestScreen.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import CustomButton from "../components/CustomButton";
-import { Entypo } from '@expo/vector-icons';
-import colors from '../colors';
+import { View, Text } from 'react-native';
 
-
-const RequestScreen = ({route, navigation}) => {
-
-    const {id, date, cupoId, passengerId, driverId} = route.params;
+const RequestScreen = () => {
 
     return(
         <View>
-            <Text>Request Screen</Text>
-            <Text>Id request: {id}</Text>
-            <Text>Fecha: {date}</Text>
-            <Text>Id cupo: {cupoId}</Text>
-            <Text>Id pasajero: {passengerId}</Text>
-            <Text>Id conductor:{driverId}</Text>
+            <Text>request</Text>
         </View>
     )
 }
 
 export default RequestScreen;
+
