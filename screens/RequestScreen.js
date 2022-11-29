ce6781cdcd29fbb139702c9e5e5b3b4d8d10536a
Martin-Cifuentes <marcisego@gmail.com>
2022-11-28 19:03:36 -0500

    Home driver

---
+++ screens/RequestScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const RequestScreen = () => {
+
+    return(
+        <View>
+            <Text>request</Text>
+        </View>
+    )
+}
+
+export default RequestScreen;
+
