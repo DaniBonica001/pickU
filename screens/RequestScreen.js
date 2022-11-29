9dd9b739bd25edcb351b8439216dc076c7666ae9
G-Delgado <gabriel.delgado1@u.icesi.edu.co>
2022-11-28 21:03:11 -0500

    Edited RequestScreen

--- screens/RequestScreen.js
+++ screens/RequestScreen.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import CustomButton from "../components/CustomButton";
+import { Entypo } from '@expo/vector-icons';
+import colors from '../colors';
 
-const RequestScreen = () => {
+
+const RequestScreen = ({route, navigation}) => {
+
+    const {id, date, cupoId, passengerId, driverId} = route.params;
 
     return(
         <View>
-            <Text>request</Text>
+            <Text>Request Screen</Text>
+            <Text>Id request: {id}</Text>
+            <Text>Fecha: {date}</Text>
+            <Text>Id cupo: {cupoId}</Text>
+            <Text>Id pasajero: {passengerId}</Text>
+            <Text>Id conductor:{driverId}</Text>
         </View>
     )
 }
 
 export default RequestScreen;
-
