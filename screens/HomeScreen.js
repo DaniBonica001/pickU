eb4ffac052b568916e611cc1ea8df0db1fb11dbb
G-Delgado <gabriel.delgado1@u.icesi.edu.co>
2022-11-28 22:12:31 -0500

    Added accept, reject buttons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,83 +1,88 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import colors from '../colors';
-import { Entypo } from '@expo/vector-icons';
-import {db} from '../firebase.js';
-import { ListItem, Avatar} from '@rneui/base';
+import colors from "../colors";
+import { Entypo } from "@expo/vector-icons";
+import { db } from "../firebase.js";
+import { ListItem, Avatar } from "@rneui/base";
 import { logInUser } from "./SignIn";
 
-const HomeScreen = ({navigation}) => {
-
+const HomeScreen = ({ navigation }) => {
   const [cupos, setCupos] = useState([]);
 
   useEffect(() => {
-    db.collection('cupo').onSnapshot(querySnapshot => {
+    db.collection("cupo").onSnapshot((querySnapshot) => {
       const cupos = [];
-      querySnapshot.docs.forEach(doc => {
-        const {date, beginning, arrive, driverId, passengers, spaces, car, carId} = doc.data()
-        cupos.push({
-          id: doc.id,
-          date, 
-          beginning, 
-          arrive, 
-          driverId, 
+      querySnapshot.docs.forEach((doc) => {
+        const {
+          date,
+          beginning,
+          arrive,
+          driverId,
           passengers,
           spaces,
           car,
           carId,
-        })
-      })
-      setCupos(cupos)
-    })
-
-  })
+        } = doc.data();
+        cupos.push({
+          id: doc.id,
+          date,
+          beginning,
+          arrive,
+          driverId,
+          passengers,
+          spaces,
+          car,
+          carId,
+        });
+      });
+      setCupos(cupos);
+    });
+  });
 
   const handleList = () => {
     console.log(cupos);
-  }
+  };
 
   return (
     <SafeAreaView style={styles.root}>
       <Text>Cupos Disponibles</Text>
 
       <View>
-        {
-          cupos.map(cupo => {
-            return(
-              <ListItem
-                key={cupo.id} onPress={() => navigation.navigate("CupoScreen", {
-                 cupo,
-                })}
-              >
-                <ListItem.Chevron/>
-                <ListItem.Content>
-                  <ListItem.Title>{cupo.arrive}</ListItem.Title>
-                </ListItem.Content>
-              </ListItem>
-            )
-          })
-        }
+        {cupos.map((cupo) => {
+          return (
+            <ListItem
+              key={cupo.id}
+              onPress={() =>
+                navigation.navigate("CupoScreen", {
+                  cupo,
+                })
+              }
+            >
+              <ListItem.Chevron />
+              <ListItem.Content>
+                <ListItem.Title>{cupo.arrive}</ListItem.Title>
+              </ListItem.Content>
+            </ListItem>
+          );
+        })}
       </View>
 
       <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() => handleList()}
+          style={styles.chatButton}
+        >
+          <Entypo name="list" size={24} color={colors.lightGray} />
+        </TouchableOpacity>
 
-            <TouchableOpacity
-                onPress={() => handleList()}
-                style={styles.chatButton}
-            >
-                <Entypo name="list" size={24} color={colors.lightGray} />
-            </TouchableOpacity>
-
-            <TouchableOpacity
-                onPress={() => navigation.navigate("ProfileScreen")}
-                style={styles.chatButton}
-            >
-                <Entypo name="user" size={24} color={colors.lightGray} />
-            </TouchableOpacity>
-        </View>
-        
-
+        <TouchableOpacity
+          onPress={() => navigation.navigate("ProfileScreen")}
+          style={styles.chatButton}
+        >
+          <Entypo name="user" size={24} color={colors.lightGray} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
@@ -90,21 +95,20 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     borderRadius: 25,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     shadowColor: colors.primary,
     shadowOffset: {
-        width: 0,
-        height: 2,
+      width: 0,
+      height: 2,
     },
-    shadowOpacity: .9,
+    shadowOpacity: 0.9,
     shadowRadius: 8,
     marginRight: 20,
     marginBottom: 50,
   },
-  container:  {
-    flex: 1,
-    flexDirection:  'row',
-    alignSelf: 'flex-end'
-  }
+  container: {
+    flexDirection: "row",
+    alignSelf: "flex-end",
+  },
 });
--- screens/RequestScreen.js
+++ screens/RequestScreen.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import CustomButton from "../components/CustomButton";
-import { Entypo } from '@expo/vector-icons';
-import colors from '../colors';
+import { Entypo } from "@expo/vector-icons";
+import colors from "../colors";
 
+const RequestScreen = ({ route, navigation }) => {
+  const { id, date, cupoId, passengerId, driverId } = route.params;
 
-const RequestScreen = ({route, navigation}) => {
+  return (
+    <View>
+      <Text>Request Screen</Text>
+      <Text>Id request: {id}</Text>
+      <Text>Fecha: {date}</Text>
+      <Text>Id cupo: {cupoId}</Text>
+      <Text>Id pasajero: {passengerId}</Text>
+      <Text>Id conductor:{driverId}</Text>
 
-    const {id, date, cupoId, passengerId, driverId} = route.params;
+      <View style={styles.container}>
+        <CustomButton
+          text="Aceptar"
+          onPress={() => console.log("sape")}
+          bgColor="#2EF181"
+          fgColor="#131530"
+        />
 
-    return(
-        <View>
-            <Text>Request Screen</Text>
-            <Text>Id request: {id}</Text>
-            <Text>Fecha: {date}</Text>
-            <Text>Id cupo: {cupoId}</Text>
-            <Text>Id pasajero: {passengerId}</Text>
-            <Text>Id conductor:{driverId}</Text>
-        </View>
-    )
-}
+        <CustomButton
+          text="Rechazar"
+          onPress={() => console.log("sape")}
+          bgColor="#FA432D"
+          fgColor="#131530"
+        />
+      </View>
+    </View>
+  );
+};
 
 export default RequestScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+});
